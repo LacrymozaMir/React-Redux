c277ee54dd268daf4e940288ce758540a08ad5a2
Danila <easyezyes@gmail.com>
2024-07-30 08:43:56 +0300

    feat(css): added blobal styles and some base component

---
+++ src/styles/components.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const AppContainer = styled.main`
+    display: flex;
+    justify-content: center;
+    max-width: 800px;
+`
+
+export const flexContainer = styled.div`
+    display: flex;
+    padding: 8px;
+    margin: 8px;
+`
---
+++ src/styles/fragments.ts
@@ -0,0 +1,12 @@
+import { css } from "styled-components";
+
+
+export const flexColumn = css`
+    display: flex;
+    flex-direction: column;
+`
+
+export const flexColumnCenter = css`
+    ${flexColumn};
+    justify-content: center;
+`
